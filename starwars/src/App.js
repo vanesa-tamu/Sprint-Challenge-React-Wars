9a51123bae1689e14402303692e9ec7bfd72a439
vanesa-tamu <vahe5495@tamu.edu>
2020-02-21 11:12:07 -0600

    cards rendering with info

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,6 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import axios from 'axios'
+import {
+  Card, CardImg, CardText, CardBody,
+  CardTitle, CardSubtitle, Container, Row, CardHeader
+} from 'reactstrap';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -17,14 +21,31 @@ const App = () => {
     axios.get(`https://swapi.co/api/people/?page=${page}&format=json`)
       .then(data => {
         console.log(data.data.results) //shows an array of 10
+        setData(data.data.results)
       })
       .catch(err => console.error('error in GET', err))
-  }, [])
+  }, [page])
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Container>
+        {data.map((char, index)=> {
+          return (
+            <Card key={index} >
+              <CardHeader>{char.name}</CardHeader>
+              <CardBody>
+              <CardText>Birth Year: {char.birth_year}</CardText>
+              <CardText>Height: {char.height}</CardText>
+              <CardText>Eye Color: {char.eye_color}</CardText>
+              </CardBody>
+            </Card>
+          )
+        })}
+      </Container>
     </div>
   );
 }
 
 export default App;
+
+//name={char.name} height={char.height} birth={char.birth_year} eye={char.eye_color}
