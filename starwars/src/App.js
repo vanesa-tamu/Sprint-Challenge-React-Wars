854de835ee0532d1e16c3bb99aa942335e4e9dad
vanesa-tamu <vahe5495@tamu.edu>
2020-02-21 10:46:16 -0600

    set up basic axios GET request

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios'
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,6 +10,16 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [data, setData] = useState([])
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    axios.get(`https://swapi.co/api/people/?page=${page}&format=json`)
+      .then(data => {
+        console.log(data.data.results) //shows an array of 10
+      })
+      .catch(err => console.error('error in GET', err))
+  }, [])
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
